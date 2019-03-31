e16707e1f03bf5c01e25d121706fd07e8565e7f3
Suraj124 <sam124.sy@gmail.com>
2019-03-31 17:10:17 +0530

    Package

--- 31-03-2019/Module_practice.py
+++ 31-03-2019/Module_practice.py
@@ -1,4 +1,11 @@
-from My_First_Module import sum,mul
+from My_Main_package import Main_Module_Animal
+from My_Main_package.My_Sub_Package import Sub_Module_Calc
 
-print(sum(2,2))
-print(mul(4,2))
+Main_Module_Animal.cat('Flixi')
+Main_Module_Animal.dog('Tulli')
+
+print('sum(a,b)',Sub_Module_Calc.sum(10,10))
+print('sub(a,b)',Sub_Module_Calc.sub(20,10))
+print('mul(a,b)',Sub_Module_Calc.mul(10,10))
+print('div(a,b)',Sub_Module_Calc.div(50,10))
+print('mod(a,b)',Sub_Module_Calc.mod(10,10))
---
+++ 31-03-2019/My_Main_package/Main_Module_Animal.py
@@ -0,0 +1,4 @@
+def cat(s):
+    print('Im cat and my name is',s)
+def dog(s):
+    print('Im dog and my name is',s)
